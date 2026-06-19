486c990aea052a0738ced9bb9122e42905e4643c
ggongchi3 <zetoty00@gmail.com>
2026-06-19 20:15:22 +0900

    Create listing_views.py

---
+++ backend/books/views/listing_views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import Listing
+from .serializers.listing_serializer import ListingSerializer
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
