a4164f40aa33b1716c807d4ce32d72b157464b56
ggongchi3 <zetoty00@gmail.com>
2026-06-19 20:15:09 +0900

    Create book_views.py

---
+++ backend/books/views/book_views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from .models import Book
+from .serializers.book_serializer import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    # 필요시 커스텀 엔드포인트 추가
+    # 예: 검색 엔드포인트를 위해 @action 사용
