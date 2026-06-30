24ad9e10164f15feeb9df4717658281fef1e5ac3
ggongchi3 <zetoty00@gmail.com>
2026-06-30 15:40:40 +0900

    Add discount_rate field to ListingSerializer

--- backend/books/serializers/listing_serializer.py
+++ backend/books/serializers/listing_serializer.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from ..models import Listing
 
 class ListingSerializer(serializers.ModelSerializer):
+    discount_rate = serializers.SerializerMethodField()
+
     class Meta:
         model = Listing
         fields = '__all__'
+        read_only_fields = ['discount_rate']
+
+    def get_discount_rate(self, obj):
+        return obj.discount_rate
