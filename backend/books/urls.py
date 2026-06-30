6f3342667abe6cdc186fe8605c865e5b8a6649f4
ggongchi3 <zetoty00@gmail.com>
2026-06-30 15:38:49 +0900

    Update urls.py

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter  # 수정: rest_framework 설치 후 사용 가능
-from .views.book_views import BookViewSet  # 수정: views/ 패키지 구조로 변경됨
-from . import views  # 수정: views/__init__.py 에서 search_book 가져옴
+from rest_framework.routers import DefaultRouter
+from .views.book_views import BookViewSet
+from .views.course_views import CourseViewSet
+from .views.listing_views import ListingViewSet
+from . import views
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet)
-# 수정: 존재하지 않는 ListingViewSet, CourseViewSet 제거 (모델 미정의)
+router.register(r'listings', ListingViewSet)
+router.register(r'courses', CourseViewSet)
 
 urlpatterns = [
     path('isbn/', views.search_book, name='search_book'),
